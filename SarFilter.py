5f27b615813a5b562f72c80a0fd19c26f1303a9b
Luis Monteiro <luiscarlos.monteiro@altran.com>
2019-08-22 18:28:50 +0100

    update: filter

--- SarFilter.py
+++ SarFilter.py
@@ -5,6 +5,10 @@
 from SarBase      import SarBase
 from SarParser    import SarParser
 from Log          import Log
+from re           import compile
+from re           import match
+from functools    import reduce
+from pprint       import pprint
 # -------------------------------------------------------------------------------------------------
 # Sar Parser
 # -------------------------------------------------------------------------------------------------
@@ -14,16 +18,49 @@ class SarFilter(SarBase):
 # -----------------------------------------------------------------------------
     # initialization
     # -------------------------------------------------------------------------
-    def __init__(self, start=None, suppress=None):
-        self.__start    = start
-        self.__suppress = suppress
-    # ------------------------------------------------------------------------- 
+    def __init__(self, target=None, filter=None):
+        self.__target = target if isinstance(target, list) else [target]
+        self.__filter = filter if isinstance(filter, list) else [filter]
+    # -------------------------------------------------------------------------
     # process
     # -------------------------------------------------------------------------
     def __call__(self, data):
-        result = data
+        print(self.__target)
+        # extract target points
+        points = reduce(
+            lambda a, p: a + list(filter(compile(p).match, data)),
+            self.__target, 
+            []
+        )
+        # filter  
+        result = {}
+        for each in points:
+            result[each] = data[each]  
+            # find connections 
+            find_inputs (data, data[each][ILINK], result)
+            find_outputs(data, data[each][OLINK], result)
+        # result 
         return result
-    #
+    # -------------------------------------------------------------------------
+    # find input 
+    # -------------------------------------------------------------------------
+    def find_inputs(data, links, results):
+        points = reduce(
+            lambda a, p: a + list(filter(compile(p).match, data)),
+            self.__filter, 
+            []
+        )
+        filter(compile(p).match, data)
+        for each in links:
+            if each in data:
+                if match(data[each][SarBase.TYPE], self.__filter):
+                    pass
+    # -------------------------------------------------------------------------
+    # find outputs 
+    # -------------------------------------------------------------------------
+    def find_inputs():
+        pass
+    
 # -----------------------------------------------------------------------------
 # private
 # -----------------------------------------------------------------------------
@@ -34,8 +71,9 @@ class SarFilter(SarBase):
 def main(params):
     # start
     Log.info('Start')
-    # xml to python
-    data = SarFilter(SarParser(params.path)())()
+    # xml to python and filter
+    SarFilter(params.target, params.filter)(SarParser(params.path)())
+    #pprint(SarFilter(params.target, params.filter)(SarParser(params.path)()))
     # finish
     Log.info('Finish')
     return 0
