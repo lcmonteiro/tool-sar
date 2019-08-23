4d22b8c0e19544db5c5f680831b0f93d8cec80a9
Monteiro <monteiro.lcm@gmail.com>
2019-08-23 14:44:04 +0100

    updade: improvement filter

--- SarFilter.py
+++ SarFilter.py
@@ -25,36 +25,49 @@ class SarFilter(SarBase):
     # process
     # -------------------------------------------------------------------------
     def __call__(self, data, cache={}):
-        # ---------------------------------------------------------------------
-        # find
-        # ---------------------------------------------------------------------
-        def find(direction, node, data, cache):
-            if direction not in node:
-                return
-            for link in node[direction]:
-                if link in cache:
-                    continue
-                if link not in data:
-                    continue
-                for pattern in self.__filter:
-                    if match(pattern, data[link][SarBase.TYPE]):
-                        continue
-                    cache[link] = data[link]
-                    if cache[link][SarBase.TYPE] == 'ASSEMBLY-SW-CONNECTOR':
-                        print(link)
-                        find(SarBase.OLINK, cache[link], data, cache)    
-                    find(direction, cache[link], data, cache)
-        # ---------------------------------------------------------------------
-        # execute
-        # ---------------------------------------------------------------------
         for target in self.__target:
             for link in filter(compile(target).match, data):
                 if link not in cache:
                     cache[link] = data[link]  
-                    # find connections 
-                    find(SarBase.ILINK, cache[link], data, cache)
-                    find(SarBase.OLINK, cache[link], data, cache)
+                    # seek connections 
+                    self.__seek(SarBase.ILINK, cache[link], data, cache)
+                    self.__seek(SarBase.OLINK, cache[link], data, cache)
         return cache
+    # -------------------------------------------------------------------------
+    # seek
+    # -------------------------------------------------------------------------
+    def __seek(self, direction, node, data, cache):
+        # seek tool
+        direction_tool = {
+            'ASSEMBLY-SW-CONNECTOR': self.__direction_connector,
+        }.get(node[SarBase.TYPE], self.__direction_default)
+        # find links
+        for link in direction_tool(direction, node):
+            if link in cache:
+                continue
+            if link not in data:
+                continue
+            for pattern in self.__filter:
+                if match(pattern, data[link][SarBase.TYPE]):
+                    continue
+                cache[link] = data[link]  
+                self.__seek(direction, cache[link], data, cache)
+    # -------------------------------------------------------------------------
+    # directions
+    # -------------------------------------------------------------------------
+    # default
+    # -----------------------------------------------------
+    def __direction_default(self, direction, node):
+        if direction not in node:
+            return
+        for link in node[direction]:
+            yield link
+    # -----------------------------------------------------
+    # connector
+    # -----------------------------------------------------
+    def __direction_connector(self, direction, node):
+        for link in node[SarBase.ILINK]:
+            yield link
 # -----------------------------------------------------------------------------
 # main
 # -----------------------------------------------------------------------------
